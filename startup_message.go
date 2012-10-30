0bbbb2cc0c8c45dd0c374572a7f70678a2485a15
Peter Hellberg <peter@c7.se>
2012-10-30 23:30:11 +0100

    Align the values

--- startup_message.go
+++ startup_message.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func printStartupMessage() {
-	fmt.Println("Loadbalancer is starting up\n")
-	fmt.Printf("port: %d\n", *port)
-	fmt.Printf("geoipPath: %s\n", *geoipPath)
-	fmt.Printf("redisKey: %s\n", *redisKey)
+	fmt.Println("Loadbalancer is starting up!\n")
+
+	fmt.Printf("port:        %d\n", *port)
+	fmt.Printf("geoipPath:   %s\n", *geoipPath)
+	fmt.Printf("redisKey:    %s\n", *redisKey)
 	fmt.Printf("redisServer: %s\n\n", *redisServer)
 }
