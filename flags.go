ee3f7a5d307e7b5f3d67efdb3be84f07953c89ce
Peter Hellberg <peter@c7.se>
2014-08-06 10:42:00 +0200

    const defaultGeoIpPath should be defaultGeoIPPath

--- flags.go
+++ flags.go
@@ -8,13 +8,13 @@ import (
 
 const (
 	defaultPort        = 1983
-	defaultGeoIpPath   = "/usr/local/share/GeoIP/GeoIP.dat"
+	defaultGeoIPPath   = "/usr/local/share/GeoIP/GeoIP.dat"
 	defaultRedisServer = "127.0.0.1:6379"
 	defaultRedisKey    = "redirect_ip"
 )
 
 var port = flag.Int("port", defaultPort, "The port number")
-var geoipPath = flag.String("geoip_path", defaultGeoIpPath, "Full path of the GeoIP.dat")
+var geoipPath = flag.String("geoip_path", defaultGeoIPPath, "Full path of the GeoIP.dat")
 var redisServer = flag.String("redis_server", defaultRedisServer, "The Redis server to use")
 var redisKey = flag.String("redis_key", defaultRedisKey, "The Redis key to use")
 
